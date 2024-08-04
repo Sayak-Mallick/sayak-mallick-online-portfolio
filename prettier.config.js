95a14429cab9eef6485ee2cc43dfb4dc5be41e43
sayakmallick4875 <sayak@solivatech.com>
2024-08-04 18:29:22 +0530

    [ TASK ] : Add project configuration files

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+const options = {
+  arrowParens: 'avoid',
+  singleQuote: true,
+  bracketSpacing: true,
+  endofline: 'lf',
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+}
+
+
+module.exports = options
